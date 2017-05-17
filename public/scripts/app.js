ddbc8ec0de4930165b960edef0e8b601b319c0a1
Derrel Wong <derrellwong@gmail.com>
2017-05-17 21:13:03 +0000

    form validation animation

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,51 +1,5 @@
 $(document).ready(function() {
 
-// var data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//       },
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-//         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-//         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-//       },
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   },
-//   {
-//     "user": {
-//       "name": "Johann von Goethe",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-//         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-//         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-//       },
-//       "handle": "@johann49"
-//     },
-//     "content": {
-//       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-//     },
-//     "created_at": 1461113796368
-//   }
-// ];
 function createTweetElement(tweet) {
   const tweetElement = $("<article>").addClass("tweet");
 
@@ -54,7 +8,6 @@ function createTweetElement(tweet) {
    const $avatar = $("<img>").attr('src', tweet.user.avatars.small).addClass('avatar');
   const $name = $("<h2>").text(tweet.user.name).addClass("name");
   const $handle = $("<span>").text(tweet.user.handle).addClass('handle');
-  console.log($handle);
   $header.append($avatar, $name, $handle);
 
   //retrieve body info
@@ -75,8 +28,7 @@ function createTweetElement(tweet) {
 
 function renderTweets(tweets) {
   //clears the tweet container before appending tweets
-  $('.tweets').empty();
-
+  // $('.tweets').empty();
   for (let tweet of tweets) {
     $('#tweets-container').append(createTweetElement(tweet))
   }
@@ -86,6 +38,29 @@ function renderTweets(tweets) {
 
 $('form').submit(function(event) {
   event.preventDefault();
+
+  let $content = $('.new-tweet textarea').val()
+
+ //form validation
+  if ($content === '') {
+      $('.new-tweet .too-short').fadeIn(function() {
+        setTimeout(function() {
+          $('.too-short').fadeOut();
+        }, 2000);
+      });
+      return;
+    }
+
+  if ($content.length > 140) {
+    $('.new-tweet .too-long').fadeIn(function() {
+      setTimeout(function() {
+        $('.too-long').fadeOut();
+      }, 2000);
+    });
+      return;
+  }
+
+
 
   $.ajax({
     data: $(this).serialize(),
@@ -109,7 +84,7 @@ function loadTweets() {
   })
 }
 
-//renderTweets(data);
 loadTweets();
 
 }); //end
+
